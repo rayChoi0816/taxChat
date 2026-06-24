20a03b80cadcdc94df98b5c16884350a817b2bb3
ray <choiminyong0816@gmail.com>
2026-06-24 21:40:59 +0900

    2606242140

---
+++ server/inc_dec2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a = 1, b;
+
+	b = ++a;
+
+	printf("a의 값은 %d\n", a);
+	printf("b의 값은 %d\n", b);
+	printf("\n");
+
+	a = 1;
+	b = a++;
+
+	printf("a의 값은 %d\n", a);
+	printf("b의 값은 %d\n", b);
+}
